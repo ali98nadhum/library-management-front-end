fed1d3d658433826bac616ad4aec5c477d1c81de
Ali nadhum <alinadhum1998@gmail.com>
2025-04-01 16:48:55 +0300

    edit layot

--- src/utils/Layout/Layout.jsx
+++ src/utils/Layout/Layout.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import {Outlet , useLocation} from "react-router"
-import Header from "../../utils/Header/Header";
 import SideBar from '../SideBar/SideBar';
 
 const Layout = () => {
@@ -8,7 +7,6 @@ const Layout = () => {
     const hideHeaderPaths = ["/auth/login"];
   return (
     <>
-    {!hideHeaderPaths.includes(location.pathname) && <Header />}
     <div className='flex'>
     {!hideHeaderPaths.includes(location.pathname) && <SideBar />}
     <Outlet />
