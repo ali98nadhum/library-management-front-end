e8baffac74b216d16f6bb8793a92ed03b5dc75e5
Ali nadhum <alinadhum1998@gmail.com>
2025-04-01 16:07:59 +0300

    add sideBar comp

--- src/utils/Layout/Layout.jsx
+++ src/utils/Layout/Layout.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import {Outlet , useLocation} from "react-router"
 import Header from "../../utils/Header/Header";
+import SideBar from '../SideBar/SideBar';
 
 const Layout = () => {
     const location = useLocation();
     const hideHeaderPaths = ["/auth/login"];
   return (
     <>
-    {!hideHeaderPaths.includes(location.pathname) && <Header />}
+    {/* {!hideHeaderPaths.includes(location.pathname) && <Header />} */}
+    {!hideHeaderPaths.includes(location.pathname) && <SideBar />}
     <Outlet />
   </>
   )
